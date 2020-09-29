5ce0d4fbb2496418740f31c16c2cf91b4418aaac
mreyers <matthewreyers3333@gmail.com>
2020-09-28 23:22:17 -0700

    Additional data and priors for eventual Bayesian approach

--- madden_19.R
+++ madden_19.R
@@ -103,11 +103,21 @@ curr_receiving_19 <- receiving_19 %>%
   group_by(receiver_player_id) %>%
   summarize_all(sum)
 
-curr_receiving_19 %>%
+receiving_plot_data <- curr_receiving_19 %>%
   left_join(receivers_19, by = c("receiver_player_id" = "nflfastR_id")) %>%
   filter(rec_pos == "WR") %>%
-  mutate(starter = depth_chart_order == 1,
-         percentile = ovr_rating %/% 10 * 10,
+  mutate(starter = depth_chart_order == 1)
+
+lm_check <- lm(tot_fant_pts ~ ovr_rating, data = receiving_plot_data)
+summary(lm_check)
+
+receiving_plot_data %>%
+  ggplot(aes(x = tot_fant_pts, y = ovr_rating, col = ovr_rating, shape = starter)) +
+  geom_point() +
+  geom_smooth(method = "lm") 
+
+receiving_plot_data %>%
+  mutate(percentile = ovr_rating %/% 10 * 10,
          percentile = factor(percentile)) %>%
   ggplot(aes(x = tot_fant_pts, col = percentile, fill = percentile)) +
   geom_density() +
@@ -116,3 +126,103 @@ curr_receiving_19 %>%
 # So these are mixture distributions, even within percentiles
 # I think these differences come from Injured / Not Injured
 # I dont have injury data here so I would have to do some sort of mixture distribution
+
+# How could I manage this?
+# Perhaps it is informed by the previous season of data?
+pbp_2018 <- readRDS(
+  url("https://raw.githubusercontent.com/guga31bb/nflfastR-data/master/data/play_by_play_2018.rds")
+  ) %>%
+  select(play_id, game_id, home_team, away_team, week, 
+         yardline_100, game_seconds_remaining, game_half, qtr,
+         down, goal_to_go, ydstogo, desc, play_type, yards_gained,
+         total_home_score, total_away_score, complete_pass, ep, epa, wp, wpa,
+         air_yards, air_epa,
+         yards_after_catch, yac_epa, comp_air_epa, comp_yac_epa,
+         fumble_lost, interception, two_point_conv_result, touchdown, pass_touchdown,
+         receiver_player_id, rusher_player_id, passer_player_id,
+         receiver_player_name, rusher_player_name, passer_player_name) %>%
+  mutate(two_point_conv_result = if_else(two_point_conv_result %in% "success", 1, 0))
+
+pass_2018 <- pbp_2018 %>%
+  filter(play_type %in% "pass")
+rush_2018 <- pbp_2018 %>%
+  filter(play_type %in% "run")
+st_and_penalties_18 <- pbp_2018 %>%
+  filter(!(play_type %in% c("pass", "run")))
+
+# 2018 receiving stuff, lets see if we can figure out why they got their madden grades
+receiving_18 <- pass_2018 %>%
+  select(game_id, play_id, week, yardline_100, down, wp, receiver_player_id, receiver_player_name,
+         yards_gained, touchdown,
+         epa, complete_pass, air_yards, air_epa, yards_after_catch, yac_epa) %>%
+  mutate(fant_pts = 0.1 * yards_gained + 0.5 * complete_pass + 6 * touchdown) %>%
+  filter(!is.na(receiver_player_id)) %>%
+  group_by(receiver_player_id, week) %>%
+  summarize(receiver_player_name = first(receiver_player_name),
+            tot_targets = n(),
+            tot_completions = sum(complete_pass, na.rm = TRUE),
+            tot_epa = sum(epa, na.rm = TRUE),
+            tot_air_epa = sum(air_epa, na.rm = TRUE),
+            tot_air_yards = sum(air_yards, na.rm = TRUE),
+            tot_yac_epa = sum(yac_epa, na.rm = TRUE),
+            tot_yac = sum(yards_after_catch, na.rm = TRUE),
+            tot_yards = sum(yards_gained, na.rm = TRUE),
+            tot_fant_pts = sum(fant_pts, na.rm = TRUE)) %>%
+  arrange(desc(tot_epa))
+
+full_send <- receiving_18 %>%
+  mutate(season = 2018) %>%
+  left_join(receivers_19, by = c("receiver_player_id" = "nflfastR_id")) %>%
+  bind_rows(receiving_19 %>%
+              left_join(receivers_19, by = c("receiver_player_id" = "nflfastR_id")) %>%
+              mutate(season = 2019))
+
+two_seasons <- full_send %>%
+  group_by(receiver_player_id, season) %>%
+  summarize(name = first(receiver_player_name),
+            pos = first(rec_pos),
+            madden_rating = first(ovr_rating),
+            tot_targets_szn = sum(tot_targets),
+            sd_targets = sd(tot_targets),
+            tot_epa_szn = sum(tot_epa),
+            sd_epa = sd(tot_epa),
+            tot_air_yards_szn = sum(tot_air_yards),
+            sd_air_yards = sd(tot_air_yards),
+            tot_fant_pts_szn = sum(tot_fant_pts),
+            sd_fant_pts = sd(tot_fant_pts))
+
+two_seasons %>%
+  filter( pos %in% "WR") %>%
+  ggplot(aes(x = tot_fant_pts_szn, y = madden_rating, col = madden_rating)) +
+  geom_point() +
+  geom_smooth() +
+  facet_wrap( ~ season)
+
+# These must be some 0.7 R^2 type fits, after removing the one outlier
+lm_2018 <- lm(madden_rating ~ tot_fant_pts_szn, data = two_seasons %>%
+                filter(season == 2018, madden_rating > 60, pos %in% "WR")) # 0.8646
+lm_2019 <- lm(madden_rating ~ tot_fant_pts_szn, data = two_seasons %>%
+                filter(season == 2019, madden_rating > 60, pos %in% "WR")) # 0.5653
+
+# So the only difference seems to be, again, the players that got hurt
+# It does seem that for at least the first few games I could easily build posteriors
+# that are heavily upweighted by the priors, where the priors are centred about
+# the madden rating in some way. A starting point may just be to use the lm
+# coef on tot_fant_pts / 16
+per_game <- two_seasons %>%
+  filter(season == 2018, madden_rating > 60, pos %in% "WR") %>%
+  mutate(per_16_fant = tot_fant_pts_szn / 16)
+
+prior_centres <- lm(per_16_fant ~ madden_rating, data = per_game)
+# So madden_rating is related to per_game fantasy points at a clip of
+# -35.5 + 0.543 * rating
+
+# Now to simply build prior centres for each player then
+real_prior_centres <- predict(prior_centres, per_game)
+
+per_game_2018_centres <- per_game %>%
+  ungroup() %>%
+  mutate(centres = real_prior_centres)
+
+# Now that I have the centres, can I build a Bayesian model to update off this as games go by?
+# To be determined
